6aa7566e2cb478703dd0bdd9e9014d89189e2a93
Lucas Santana <lucassantan017@gmail.com>
2025-07-12 23:37:07 -0300

    Paralelismo em docker

--- src/test/java/br/qa/lcsantana/tasks/functional/TaskTest.java
+++ src/test/java/br/qa/lcsantana/tasks/functional/TaskTest.java
@@ -1,85 +1,98 @@
 package br.qa.lcsantana.tasks.functional;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+
 public class TaskTest {
 
-    private static WebDriver driver;
+    private static final Duration tempo = Duration.ofSeconds(10);
 
-    @Before
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        if (driver == null) {
-            driver = new ChromeDriver();
-        }
-        driver.get("http://localhost:8001/tasks");
+    public RemoteWebDriver acessarAplicacao() throws MalformedURLException, URISyntaxException {
+        ChromeOptions options = new ChromeOptions();
+        RemoteWebDriver driver = new RemoteWebDriver(new URI("http://192.168.1.4:4444/").toURL(), options);
+        driver.get("http://192.168.1.4:8001/tasks");
+        return driver;
     }
 
-    @After
-    public void teardown() {
-        if (driver != null) {
+    @Test
+    public void deveSalvarTarefaComSucesso() throws MalformedURLException, URISyntaxException {
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste funcional selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("12/07/2025");
+            driver.findElement(By.id("saveButton")).click();
+
+            WebDriverWait wait = new WebDriverWait(driver, tempo);
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+            Assert.assertEquals("Success!", driver.findElement(By.id("message")).getText());
+        }finally {
             driver.quit();
-            driver = null;
         }
     }
 
     @Test
-    public void deveSalvarTarefaComSucesso() {
-        driver.findElement(By.id("addTodo")).click();
-        driver.findElement(By.id("task")).sendKeys("Teste funcional selenium");
-        driver.findElement(By.id("dueDate")).sendKeys("12/07/2025");
-        driver.findElement(By.id("saveButton")).click();
+    public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException, URISyntaxException {
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("dueDate")).sendKeys("12/07/2025");
+            driver.findElement(By.id("saveButton")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebDriverWait wait = new WebDriverWait(driver, tempo);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
-        Assert.assertEquals("Success!", driver.findElement(By.id("message")).getText());
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+            Assert.assertEquals("Fill the task description", driver.findElement(By.id("message")).getText());
+        }finally {
+          driver.quit();
+        }
     }
 
     @Test
-    public void naoDeveSalvarTarefaSemDescricao() {
-        driver.findElement(By.id("addTodo")).click();
-        driver.findElement(By.id("dueDate")).sendKeys("12/07/2025");
-        driver.findElement(By.id("saveButton")).click();
+    public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException, URISyntaxException {
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste funcional selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("12/07/2020");
+            driver.findElement(By.id("saveButton")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebDriverWait wait = new WebDriverWait(driver, tempo);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
-        Assert.assertEquals("Fill the task description", driver.findElement(By.id("message")).getText());
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+            Assert.assertEquals("Due date must not be in past", driver.findElement(By.id("message")).getText());
+        }finally {
+            driver.quit();
+        }
     }
 
     @Test
-    public void naoDeveSalvarTarefaComDataPassada() {
-        driver.findElement(By.id("addTodo")).click();
-        driver.findElement(By.id("task")).sendKeys("Teste funcional selenium");
-        driver.findElement(By.id("dueDate")).sendKeys("12/07/2020");
-        driver.findElement(By.id("saveButton")).click();
+    public void naoDeveSalvarTarefaSemData() throws MalformedURLException, URISyntaxException {
+        WebDriver driver = acessarAplicacao();
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Teste funcional selenium");
+            driver.findElement(By.id("dueDate")).sendKeys("");
+            driver.findElement(By.id("saveButton")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebDriverWait wait = new WebDriverWait(driver, tempo);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
-        Assert.assertEquals("Due date must not be in past", driver.findElement(By.id("message")).getText());
-    }
-
-    @Test
-    public void naoDeveSalvarTarefaSemData() {
-        driver.findElement(By.id("addTodo")).click();
-        driver.findElement(By.id("task")).sendKeys("Teste funcional selenium");
-        driver.findElement(By.id("dueDate")).sendKeys("");
-        driver.findElement(By.id("saveButton")).click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
-        Assert.assertEquals("Fill the due date", driver.findElement(By.id("message")).getText());
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+            Assert.assertEquals("Fill the due date", driver.findElement(By.id("message")).getText());
+        } finally {
+            driver.quit();
+        }
     }
 }
